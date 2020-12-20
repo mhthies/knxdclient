a8313f221dd5b4b285f6d071fff3d64b481acd5b
Michael Thies <mail@mhthies.de>
2020-12-20 14:48:12 +0100

    Bump version to 0.3.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="knxdclient",
-    version="0.3.0",
+    version="0.3.1",
     author="Michael Thies",
     author_email="mail@mhthies.de",
     url="https://github.com/mhthies/knxdclient",
