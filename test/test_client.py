229c2dec12f562de5a9815b90feaa342954349c3
Michael Thies <mail@mhthies.de>
2022-10-13 11:53:57 +0200

    Add more client tests

--- test/test_client.py
+++ test/test_client.py
@@ -14,7 +14,7 @@ from ._helper import async_test
 
 @unittest.skipIf(shutil.which("knxd") is None, "knxd is not available in PATH")
 @unittest.skipIf(shutil.which("knxtool") is None, "knxtool is not available in PATH")
-class MQTTClientTest(unittest.TestCase):
+class KNXDClientTest(unittest.TestCase):
     def setUp(self) -> None:
         self.knxd_socket = tempfile.mktemp(suffix=".sock", prefix="knxdclient-test-knxd-")
         self.knxd_process = subprocess.Popen(["knxd", f"--listen-local={self.knxd_socket}", "-e", "0.5.1", "-E",
@@ -118,6 +118,138 @@ class MQTTClientTest(unittest.TestCase):
             with suppress(asyncio.CancelledError):
                 await run_task
 
-    # TODO test exception on connection failure
-    # TODO test iterate_group_telegrams()
-    # TODO test reconnect
+    @async_test
+    async def test_connection_failure(self) -> None:
+        # Setup connection and receive loop task
+        connection = knxdclient.KNXDConnection()
+        await connection.connect(sock=self.knxd_socket)
+        run_task = asyncio.create_task(connection.run())
+
+        # Open Group socket
+        await connection.open_group_socket()
+
+        # Now, let's stop KNXD
+        self.knxd_process.terminate()
+        self.knxd_process.wait()  # This is thread-blocking, which should not be a problem here
+
+        # This should raise an exception in the run_task (receive loop)
+        with self.assertRaises(ConnectionAbortedError):
+            await run_task
+
+        # Let's restart KNXD
+        self.knxd_process = subprocess.Popen(["knxd", f"--listen-local={self.knxd_socket}", "-e", "0.5.1", "-E",
+                                              "0.5.2:10", "dummy"])
+        time.sleep(0.25)  # This is thread-blocking, which should not be a problem here
+
+        # Now, we should be able to reconnect the client and open the group socket
+        await connection.connect(sock=self.knxd_socket)
+        run_task = asyncio.create_task(connection.run())
+        try:
+            await connection.open_group_socket()
+        finally:
+            await connection.stop()
+            with suppress(asyncio.CancelledError):
+                await run_task
+
+    @async_test
+    async def test_receive_iterator(self) -> None:
+        # Setup connection and receive loop task
+        connection = knxdclient.KNXDConnection()
+        await connection.connect(sock=self.knxd_socket)
+        run_task = asyncio.create_task(connection.run())
+
+        try:
+            handler = unittest.mock.Mock()
+
+            async def iter_packets():
+                async for packet in connection.iterate_group_telegrams():
+                    handler(packet)
+            iterator_task = asyncio.create_task(iter_packets())
+            # let task start
+            await asyncio.sleep(0.05)
+
+            try:
+                # A second iterator should not be allowed
+                with self.assertRaises(RuntimeError):
+                    _telegram_iterator2 = await connection.iterate_group_telegrams().__aiter__().__anext__()
+
+                # Open Group socket
+                await connection.open_group_socket()
+                await asyncio.sleep(0.05)
+
+                proc = await asyncio.create_subprocess_exec('knxtool', 'on', f'local://{self.knxd_socket}', '1/2/3',
+                                                            env=dict(PATH=os.environ['PATH']))
+                await proc.communicate()
+                if proc.returncode:
+                    raise RuntimeError(f"knxtool failed with exit code {proc.returncode}")
+                handler.assert_called_once()
+                value1 = handler.call_args[0][0]
+
+                proc = await asyncio.create_subprocess_exec('knxtool', 'groupwrite', f'local://{self.knxd_socket}',
+                                                            '17/5/127', 'de', 'ad', 'c0', 'ff', 'ee',
+                                                            env=dict(PATH=os.environ['PATH']))
+                await proc.communicate()
+                if proc.returncode:
+                    raise RuntimeError(f"knxtool failed with exit code {proc.returncode}")
+
+                self.assertEqual(2, handler.call_count)
+                value2 = handler.call_args[0][0]
+
+            except Exception:
+                iterator_task.cancel()
+                await iterator_task
+                raise
+
+            # Trigger a CancelledError in the iterator to ensure correct handling
+            iterator_task.cancel()
+            with self.assertRaises(asyncio.CancelledError):
+                await iterator_task
+            # This should allow us to create a new iterator
+            _telegram_iterator2 = connection.iterate_group_telegrams()
+
+            # Check received values
+            self.assertIsInstance(value1, knxdclient.ReceivedGroupAPDU)
+            self.assertEqual(knxdclient.GroupAddress(1, 2, 3), value1.dst)
+            self.assertEqual(0, value1.src.area)
+            self.assertEqual(5, value1.src.line)
+            self.assertEqual(knxdclient.KNXGroupAPDU(knxdclient.KNXDAPDUType.WRITE, 0x1),
+                             value1.payload)
+            self.assertIsInstance(value2, knxdclient.ReceivedGroupAPDU)
+            self.assertEqual(knxdclient.GroupAddress(17, 5, 127), value2.dst)
+            self.assertEqual(0, value2.src.area)
+            self.assertEqual(5, value2.src.line)
+            self.assertEqual(knxdclient.KNXGroupAPDU(knxdclient.KNXDAPDUType.WRITE,
+                                                     bytes([0xde, 0xad, 0xc0, 0xff, 0xee])),
+                             value2.payload)
+
+        finally:
+            await connection.stop()
+            with suppress(asyncio.CancelledError):
+                await run_task
+
+
+class KNXDClientTCPTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.knxd_process = subprocess.Popen(["knxd", "--listen-tcp=16720", "-e", "0.5.1", "-E",
+                                              "0.5.2:10", "dummy"])
+        time.sleep(0.25)
+
+    def tearDown(self) -> None:
+        self.knxd_process.terminate()
+        self.knxd_process.wait()
+
+    @async_test
+    async def test_connect_tcp(self) -> None:
+        # Setup connection and receive loop task
+        connection = knxdclient.KNXDConnection()
+        await connection.connect(host="127.0.0.1", port=16720)
+        run_task = asyncio.create_task(connection.run())
+
+        try:
+            # Open Group socket
+            await connection.open_group_socket()
+
+        finally:
+            await connection.stop()
+            with suppress(asyncio.CancelledError):
+                await run_task
