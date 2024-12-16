be6549c40eb711d9b4335ad0e1c01253e08821e6
Greig Sheridan <greiginsydney@users.noreply.github.com>
2024-12-17 08:20:36 +1100

    Added standalone tests

--- test/test_conversion.py
+++ test/test_conversion.py
@@ -275,3 +275,7 @@ class MQTTClientTest(unittest.TestCase):
         self.assertEqual((0x12, 0x34, 0x56),
                          knxdclient.decode_value(knxdclient.encode_value((0x12, 0x34, 0x56), knxdclient.KNXDPT.COLOUR_RGB),
                                                  knxdclient.KNXDPT.COLOUR_RGB))
+        self.assertEqual(bytes([0x7f, 0x00, 0x20]),
+                         knxdclient.encode_value((0x7f, 0x00, 0x20), knxdclient.KNXDPT.COLOUR_RGB))
+        self.assertEqual((0x10, 0xcc, 0x0a),
+                         knxdclient.decode_value(bytes([0x10, 0xcc, 0x0a]), knxdclient.KNXDPT.COLOUR_RGB))
